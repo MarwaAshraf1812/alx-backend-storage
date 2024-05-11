79fd509ffc797f5e239bb0fd65b6466b1a7b5882
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-12 00:57:13 +0300

    1. In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -9,5 +9,5 @@ CREATE TABLE IF NOT EXISTS USERS (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
