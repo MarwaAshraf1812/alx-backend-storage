7f0142b30cdfe02ead1f21cb4cb3d198b18cb456
MarwaAshraf1812 <marashraf090@gmail.com>
2024-05-14 21:17:32 +0300

    7. Average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,35 +1,23 @@
 -- Creates a stored procedure ComputeAverageScoreForUser that
 -- computes and stores the average score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
-    DECLARE total_score FLOAT DEFAULT 0;
-    DECLARE project_counts INT DEFAULT 0;
-    DECLARE average_score FLOAT DEFAULT 0;
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT 0;
 
-    -- Calculate the total score for the user
     SELECT SUM(score)
-    INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
 
-    -- Calculate the number of projects
-    SELECT COUNT(DISTINCT project_id)
-    INTO project_counts
-    FROM corrections
-    WHERE user_id = user_id;
-
-    -- Calculate the average score
-    IF project_counts > 0 THEN
-        SET average_score = total_score / project_counts;
-    END IF;
-
-    -- Store the average score in the users table
     UPDATE users
-    SET average_score = average_score
-    WHERE id = user_id;
-END //
-
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END $$
 DELIMITER ;
